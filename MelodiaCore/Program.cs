76ee2b3091dd26151f9a69a05a2900ef60736eb5
Utamago <44744503+Utamago@users.noreply.github.com>
2019-07-06 18:06:04 +0800

    first commit

---
+++ MelodiaCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace MelodiaCore
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            //args = new string[0];
+
+            Console.WriteLine("Melodia here!");
+
+            /*if (args.Any() && args.First() == "--version")
+            {
+                Console.WriteLine("I'm in version 0.0.1!");
+            }*/
+        }
+    }
+}
